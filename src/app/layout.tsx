d1470463b90716ca1cebe0ffe96714c28257359a
Andries Liebenberg <create@an3s.info>
2025-12-07 19:42:13 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&family=Montserrat:wght@700&family=Space+Grotesk:wght@500&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <ScreenFrame>
           <Header />
           <main className="flex-1 overflow-y-auto">
